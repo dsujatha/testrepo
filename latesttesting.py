f998457e9d11230549d4dd12f48caea770fa5146
dsujatha <42560098+dsujatha@users.noreply.github.com>
2019-05-18 06:49:22 +0530

    Update latesttesting.py

--- latesttesting.py
+++ latesttesting.py
@@ -21,5 +21,24 @@
                 format_col_value = self.format_col_value(col,record[col])
                 row_data = row_data + (format_col_value,)
           recordData.append(row_data)
-  print(recordData)
   return recordData
+ 
+ 
+ def run(self):
+    with open("json_file.json") as f:
+        json_string = f.read()
+        try:
+            parsed_json = json.loads(json_string)
+            script = ""
+            keys = parsed_json.items()
+
+            for table_name,table_metadata in parsed_json.items():
+                script = self.build_create_table(table_name,table_metadata)
+                print(script)
+            if('records' in table_metadata):
+                base_insert_query = self.build_insert_query_template(table_name, table_metadata)
+                print(base_insert_query)
+                records_query = self.build_records(table_name,table_metadata)
+                print(records_query)
+        except Exception as e:
+            print(e)
